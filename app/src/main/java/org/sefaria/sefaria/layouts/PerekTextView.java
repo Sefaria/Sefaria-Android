b2d7c863915d8b51449f4448e1a792fc839a28ad
nsantacruz <noahssantacruz@gmail.com>
2015-11-04 09:47:28 -0500

    yo

--- app/src/main/java/org/sefaria/sefaria/layouts/PerekTextView.java
+++ app/src/main/java/org/sefaria/sefaria/layouts/PerekTextView.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Path;
 import android.graphics.Paint;
+import android.text.Html;
 import android.text.Layout;
 import android.text.SpannableString;
 import android.text.SpannableStringBuilder;
@@ -12,6 +13,7 @@ import android.text.StaticLayout;
 import android.text.TextPaint;
 import android.text.method.LinkMovementMethod;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -21,6 +23,8 @@ import org.sefaria.sefaria.database.Text;
 
 import java.text.Bidi;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by nss on 10/28/15.
@@ -62,9 +66,8 @@ public class PerekTextView extends JustifyTextView {
                 int lineEnd = layout.getLineEnd(i);
                 String line = text.substring(lineStart, lineEnd);
                 float width = StaticLayout.getDesiredWidth(text, lineStart, lineEnd, getPaint());
-                if (needScale(line) && i != layout.getLineCount()-1 ) {
-                    drawScaledText(canvas, lineStart, line, width);
-
+                if (needScale(line) && i != layout.getLineCount()-1 && true) {
+                    drawScaledText(canvas, lineStart, line, width,lang == Util.HE);
                 } else {
                     canvas.drawText(line, 0, mLineY, paint);
                 }
@@ -73,19 +76,53 @@ public class PerekTextView extends JustifyTextView {
             }
         } else { //seperate
             oldOnDraw(canvas);
-            //super.onDraw(canvas);
         }
     }
 
-    protected void drawScaledText(Canvas canvas, int lineStart, String line, float lineWidth) {
-        float x = 0;
+    protected void drawScaledText(Canvas canvas, int lineStart, String line, float lineWidth,boolean isHeb) {
+        final Pattern r = Pattern.compile(".[\u0591-\u05BD\u05BF\u05C1\u05C2\u05C4\u05C5\u05C7]*"); //find any char followed by at least 0 nikudot
+        String parenString = "([{";
+        String flipParenString = ")]}";
 
-        float d = (mViewWidth - lineWidth) / line.length() - 1;
-        for (int i = 0; i < line.length(); i++) {
-            String c = String.valueOf(line.charAt(i));
-            float cw = StaticLayout.getDesiredWidth(c, getPaint());
-            canvas.drawText(c, x, mLineY, getPaint());
-            x += cw + d;
+        //line = "(4) [3] {%} " + line;
+        Matcher m = r.matcher(line);
+        float x = isHeb ? mViewWidth : 0;
+        TextPaint tempPaint = getPaint();
+        float d;
+        if (isHeb) {
+            for (int i = 0; i < parenString.length(); i++) {
+                line = line.replace(parenString.charAt(i),'`')
+                        .replace(flipParenString.charAt(i), parenString.charAt(i))
+                        .replace('`', flipParenString.charAt(i));
+            }
+
+            Matcher tm = r.matcher(line);
+            int matchCount = 0;
+            while(tm.find())
+                matchCount++;
+
+
+            d = (mViewWidth - lineWidth) / matchCount;
+        } else {
+            d = (mViewWidth - lineWidth) / line.length() - 1;
+        }
+
+        int i = 0;
+        float cw;
+        String c;
+        while (i < line.length() && (!isHeb || m.find())) {
+            if (isHeb) {
+                c = line.substring(m.start(), m.end());
+                cw = StaticLayout.getDesiredWidth(c, tempPaint);
+                canvas.drawText(c, x-cw, mLineY, tempPaint);
+                x -= (cw + d);
+            } else {
+                c = String.valueOf(line.charAt(i));
+                cw = StaticLayout.getDesiredWidth(c, tempPaint);
+                canvas.drawText(c, x, mLineY, tempPaint);
+                x += cw + d;
+                i++;
+            }
         }
     }
 
@@ -108,6 +145,7 @@ public class PerekTextView extends JustifyTextView {
 
     public void setLang(int lang) {
         this.lang = lang;
+        if (lang == Util.BI) setIsCts(false);
         update();
 
     }
@@ -119,10 +157,13 @@ public class PerekTextView extends JustifyTextView {
         for (Text text : textList) {
             String words;
             if (lang == Util.EN) words = "(" + text.levels[0] + ") " + text.enText;
-            else words = "(" + Util.int2heb(text.levels[0]) + ") " + text.heText;
-
+            else if (lang == Util.HE) words = "(" + Util.int2heb(text.levels[0]) + ") " + text.heText;
+            else { //bilingual
+                words = "\n\n(" + Util.int2heb(text.levels[0]) + ") " + text.heText
+                + "\n\n(" + text.levels[0] + ") " + text.enText;
+            }
             words = isCts && !isFirst ? words : "\n" + words;
-            SpannableString ss = new SpannableString(words);
+            SpannableString ss = new SpannableString(Html.fromHtml(words));
             ss.setSpan(new VerseSpannable(words) ,0,ss.length(), 0);
             ssb.append(ss);
             isFirst = false;
