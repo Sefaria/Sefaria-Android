a6482f6da48e68662616988f3984780c0e58f3b2
nsantacruz <noahssantacruz@gmail.com>
2015-11-25 01:04:20 -0500

    added toc activity

---
+++ app/src/main/java/org/sefaria/sefaria/activities/TOCActivity.java
@@ -0,0 +1,48 @@
+package org.sefaria.sefaria.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import org.sefaria.sefaria.R;
+import org.sefaria.sefaria.Util;
+import org.sefaria.sefaria.database.Book;
+import org.sefaria.sefaria.layouts.CustomActionbar;
+import org.sefaria.sefaria.menu.MenuNode;
+
+public class TOCActivity extends AppCompatActivity {
+
+    private Book book;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toc);
+
+        Intent intent = getIntent();
+        book = intent.getParcelableExtra("currBook");
+        Log.d("toc", "BoOOOK " + book);
+
+        init();
+    }
+
+    private void init() {
+        MenuNode titleNode = new MenuNode("Table of Contents","תוכן העניינים",null);
+        CustomActionbar cab = new CustomActionbar(this, titleNode, Util.Lang.EN,null,closeClick,null,null);
+        LinearLayout abRoot = (LinearLayout) findViewById(R.id.actionbarRoot);
+        abRoot.addView(cab);
+    }
+
+    View.OnClickListener closeClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            finish();
+        }
+    };
+
+}
