df5fd36456e36858598cf62587ee40d4f6e281c1
nsantacruz <noahssantacruz@gmail.com>
2016-01-08 15:51:30 -0500

    non-cts verse numbers work and bilingual works

--- app/src/main/java/org/sefaria/sefaria/Util.java
+++ app/src/main/java/org/sefaria/sefaria/Util.java
@@ -35,6 +35,7 @@ public class Util {
         TEXTS,MENU,SYSTEM
     }
 
+    public static final String VERSE_BULLET = "\u25CF";
     public static final float EN_HE_RATIO = 40f/35f; //universal constant
 
     public static int dp2pixel(int dp, Context context) {
