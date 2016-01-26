b07bd68f01bfe9f939124f2cf8a78656a052db93
Josh Herzberg <joshmherzberg@gmail.com>
2016-01-25 23:56:34 -0500

    change TOC down arrow. made TextAct not crash on return

--- app/src/main/java/org/sefaria/sefaria/activities/LinkFragment.java
+++ app/src/main/java/org/sefaria/sefaria/activities/LinkFragment.java
@@ -193,7 +193,8 @@ public class LinkFragment extends Fragment {
         if (!dontUpdate || clicked) {
             this.segment = segment;
             clicked = false;
-            Log.d("frag", "UPDATE FRAG TEXT " + segment.levels[0]);
+            if(!segment.isChapter()) Log.d("frag", "UPDATE FRAG TEXT " + segment.levels[0]);
+
             if (currState == State.MAIN) { //load new linkCounts
                 Link.LinkCount linkCount = Link.LinkCount.getFromLinks_small(segment);
                 linkMainAdapter.setItemList(Link.LinkCount.getList(linkCount));
