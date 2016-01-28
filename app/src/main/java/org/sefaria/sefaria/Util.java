1da396dc44bff43d4bcfc7106423b6a978b10fbf
Josh Herzberg <joshmherzberg@gmail.com>
2016-01-28 00:02:54 -0500

    more sd fun

--- app/src/main/java/org/sefaria/sefaria/Util.java
+++ app/src/main/java/org/sefaria/sefaria/Util.java
@@ -2,6 +2,9 @@ package org.sefaria.sefaria;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.os.Build;
+import android.os.Environment;
+import android.text.TextUtils;
 import android.view.View;
 
 import org.json.JSONException;
@@ -17,8 +20,11 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Locale;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -302,4 +308,85 @@ public class Util {
         else
             return myView.getTop() + getRelativeTop((View) myView.getParent());
     }
+
+
+    private static final Pattern DIR_SEPORATOR = Pattern.compile("/");
+
+    /**
+     * Raturns all available SD-Cards in the system (include emulated)
+     *
+     * Warning: Hack! Based on Android source code of version 4.3 (API 18)
+     * Because there is no standart way to get it.
+     * TODO: Test on future Android versions 4.4+
+     *
+     * @return paths to all available SD-Cards in the system (include emulated)
+     */
+    public static String[] getStorageDirectories()
+    {
+        // Final set of paths
+        final Set<String> rv = new HashSet<String>();
+        // Primary physical SD-CARD (not emulated)
+        final String rawExternalStorage = System.getenv("EXTERNAL_STORAGE");
+        // All Secondary SD-CARDs (all exclude primary) separated by ":"
+        final String rawSecondaryStoragesStr = System.getenv("SECONDARY_STORAGE");
+        // Primary emulated SD-CARD
+        final String rawEmulatedStorageTarget = System.getenv("EMULATED_STORAGE_TARGET");
+        if(TextUtils.isEmpty(rawEmulatedStorageTarget))
+        {
+            // Device has physical external storage; use plain paths.
+            if(TextUtils.isEmpty(rawExternalStorage))
+            {
+                // EXTERNAL_STORAGE undefined; falling back to default.
+                rv.add("/storage/sdcard0");
+            }
+            else
+            {
+                rv.add(rawExternalStorage);
+            }
+        }
+        else
+        {
+            // Device has emulated storage; external storage paths should have
+            // userId burned into them.
+            final String rawUserId;
+            if(Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1)
+            {
+                rawUserId = "";
+            }
+            else
+            {
+                final String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+                final String[] folders = DIR_SEPORATOR.split(path);
+                final String lastFolder = folders[folders.length - 1];
+                boolean isDigit = false;
+                try
+                {
+                    Integer.valueOf(lastFolder);
+                    isDigit = true;
+                }
+                catch(NumberFormatException ignored)
+                {
+                }
+                rawUserId = isDigit ? lastFolder : "";
+            }
+            // /storage/emulated/0[1,2,...]
+            if(TextUtils.isEmpty(rawUserId))
+            {
+                rv.add(rawEmulatedStorageTarget);
+            }
+            else
+            {
+                rv.add(rawEmulatedStorageTarget + File.separator + rawUserId);
+            }
+        }
+        // Add all secondary storages
+        if(!TextUtils.isEmpty(rawSecondaryStoragesStr))
+        {
+            // All Secondary SD-CARDs splited into array
+            final String[] rawSecondaryStorages = rawSecondaryStoragesStr.split(File.pathSeparator);
+            Collections.addAll(rv, rawSecondaryStorages);
+        }
+        return rv.toArray(new String[rv.size()]);
+    }
+
 }
