e0d9e880718c59e4612800ac2c29a4872bdef1a5
Noah Santacruz <noahssantacruz@gmail.com>
2016-03-01 14:49:07 -0500

    made dialogs worse. works a little better for marshmallow

--- app/src/main/java/org/sefaria/sefaria/activities/HomeActivity.java
+++ app/src/main/java/org/sefaria/sefaria/activities/HomeActivity.java
@@ -237,7 +237,7 @@ public class HomeActivity extends Activity {
         LinearLayout calendarRoot = new LinearLayout(this);
         calendarRoot.setOrientation(LinearLayout.HORIZONTAL);
         calendarRoot.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
-        homeRoot.addView(createTypeTitle("Calendar",false));
+        homeRoot.addView(createTypeTitle("Calendar", false));
         homeRoot.addView(calendarRoot);
         dailtylearnings = DailyLearning.getDailyLearnings(this);
         for(MenuDirectRef menuDirectRef: dailtylearnings) {
@@ -250,7 +250,7 @@ public class HomeActivity extends Activity {
         textView.setText(title);
         final int paddingSide= 3;
         final int paddingTop = 20;
-        textView.setPadding(paddingSide,paddingTop*2,paddingSide,paddingTop);
+        textView.setPadding(paddingSide, paddingTop * 2, paddingSide, paddingTop);
         textView.setTextSize(20);
         textView.setFont(Util.Lang.EN, isSerif); //TODO change with system lang
         textView.setGravity(Gravity.CENTER);
@@ -275,11 +275,37 @@ public class HomeActivity extends Activity {
         out.putParcelable("menuState", menuState);
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MyApp.REQUEST_WRITE_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
     View.OnClickListener searchClick = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
         }
     };
+
+
 
     View.OnClickListener closeClick = new View.OnClickListener() {
         @Override
