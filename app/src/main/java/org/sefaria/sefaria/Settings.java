deca74cd49dd5e69267f49122210a5dd384cfd75
Josh Herzberg <joshmherzberg@gmail.com>
2016-03-04 10:35:05 -0500

    better Settings.Link function

--- app/src/main/java/org/sefaria/sefaria/Settings.java
+++ app/src/main/java/org/sefaria/sefaria/Settings.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -285,17 +286,29 @@ public class Settings {
             editor.commit();
         }
 
+
+        private static void parseLinkFilterTree(LinkFilter linkFilter, LinkedList<LinkFilter> list, Set<String> set){
+            if(set.contains(linkFilter.getRealTitle(Util.Lang.EN))){
+                list.add(linkFilter);
+            }
+            for(LinkFilter child:linkFilter.getChildren()){
+                parseLinkFilterTree(child,list,set);
+            }
+        }
+
         /**
          *
          * @param enBookTitle
          * @return Set of the strings that are the EN names for the linkFilters
          */
-        public static Set<String> getLinks(String enBookTitle){
+        public static LinkedList<LinkFilter> getLinks(String enBookTitle, LinkFilter linkFilterAll){
             SharedPreferences settings = getLinkSettings();
-            Set<String> set = settings.getStringSet(enBookTitle,null);
+            Set<String> set = settings.getStringSet(enBookTitle, null);
+            LinkedList<LinkFilter> list = new LinkedList<>();
             if(set == null)
-                set = new HashSet<>();
-            return set;
+                return list;
+            parseLinkFilterTree(linkFilterAll,list,set);
+            return list;
         }
 
     }
