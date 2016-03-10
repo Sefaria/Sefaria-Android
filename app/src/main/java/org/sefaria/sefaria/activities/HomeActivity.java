f5fac4ab13e5a90595fb2020a146c3aa16f11dc7
Josh Herzberg <joshmherzberg@gmail.com>
2016-03-10 17:32:44 -0500

    removed dependencies

--- app/src/main/java/org/sefaria/sefaria/activities/HomeActivity.java
+++ app/src/main/java/org/sefaria/sefaria/activities/HomeActivity.java
@@ -348,11 +348,8 @@ public class HomeActivity extends Activity {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
                     dealWithDatabaseStuff();
-
                 } else {
-
                     // permission denied, boo! Disable the
                     // functionality that depends on this permission.
                 }
