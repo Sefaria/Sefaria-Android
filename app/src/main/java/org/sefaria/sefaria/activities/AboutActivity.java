b050fdf0e63f4bf311cf8b99eaa2fa0f089ff552
Noah Santacruz <noahssantacruz@gmail.com>
2016-03-10 17:18:12 -0500

    redundant imports removed from activities

--- app/src/main/java/org/sefaria/sefaria/activities/AboutActivity.java
+++ app/src/main/java/org/sefaria/sefaria/activities/AboutActivity.java
@@ -1,15 +1,9 @@
 package org.sefaria.sefaria.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.Toast;
 
 import org.sefaria.sefaria.R;
-import org.sefaria.sefaria.database.Downloader;
 
 public class AboutActivity extends AppCompatActivity {
 
