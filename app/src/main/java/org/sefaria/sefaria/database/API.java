9cd7eae28784ec191c7f447111ad55a1871467e5
Josh Herzberg <joshmherzberg@gmail.com>
2016-04-07 17:12:18 -0400

    better sorting API links

--- app/src/main/java/org/sefaria/sefaria/database/API.java
+++ app/src/main/java/org/sefaria/sefaria/database/API.java
@@ -461,70 +461,21 @@ public class API {
             e.printStackTrace();
         }
 
-        Collections.sort(commentaries,comp);
-        Collections.sort(texts, comp);
-        commentaries.addAll(texts);
+        Collections.sort(commentaries,compareTexts);
+        Collections.sort(texts, compareTexts);
+        texts.addAll(0,commentaries);
 
         Log.d("API.Link","finished LinksAPI");
-        return commentaries;
+        return texts;
     }
 
-    static Comparator<Text> comp = new Comparator<Text>() {
+    static Comparator<Text> compareTexts = new Comparator<Text>() {
         @Override
         public int compare(Text a, Text b) {
-            if(a.bid != b.bid)
-                return a.bid - b.bid;
-            return 0;
-            //return NumberAwareStringComparator.INSTANCE.compare(a.getRef(),b.getRef());
-            //return a.getRef().compareTo(b.getRef());
+            //only sorting on bid. Within same book using sable sort to keep order
+            return a.bid - b.bid;
         }
     };
-
-    public static class NumberAwareStringComparator implements Comparator<CharSequence> {
-        public static final NumberAwareStringComparator INSTANCE =
-                new NumberAwareStringComparator();
-
-        private static final Pattern PATTERN = Pattern.compile("(\\D*)(\\d*)");
-
-        private NumberAwareStringComparator() {
-        }
-
-        public int compare(CharSequence s1, CharSequence s2) {
-            Matcher m1 = PATTERN.matcher(s1);
-            Matcher m2 = PATTERN.matcher(s2);
-
-            // The only way find() could fail is at the end of a string
-            while (m1.find() && m2.find()) {
-                // matcher.group(1) fetches any non-digits captured by the
-                // first parentheses in PATTERN.
-                int nonDigitCompare = m1.group(1).compareTo(m2.group(1));
-                if (0 != nonDigitCompare) {
-                    return nonDigitCompare;
-                }
-
-                // matcher.group(2) fetches any digits captured by the
-                // second parentheses in PATTERN.
-                if (m1.group(2).isEmpty()) {
-                    return m2.group(2).isEmpty() ? 0 : -1;
-                } else if (m2.group(2).isEmpty()) {
-                    return +1;
-                }
-
-                BigInteger n1 = new BigInteger(m1.group(2));
-                BigInteger n2 = new BigInteger(m2.group(2));
-                int numberCompare = n1.compareTo(n2);
-                if (0 != numberCompare) {
-                    return numberCompare;
-                }
-            }
-
-            // Handle if one string is a prefix of the other.
-            // Nothing comes before something.
-            return m1.hitEnd() && m2.hitEnd() ? 0 :
-                    m1.hitEnd()                ? -1 : +1;
-        }
-    };
-
 
     static private String createPlace(String bookTitle, int[] levels){
         String place = bookTitle.replace(" ", "_"); //the api call doesn't have spaces
