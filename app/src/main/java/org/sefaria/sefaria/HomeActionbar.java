bf317c4bc37bf26456aa78018344b064bda5df38
Noah Santacruz <noahssantacruz@gmail.com>
2016-04-11 01:10:11 -0400

    home drawer done

---
+++ app/src/main/java/org/sefaria/sefaria/HomeActionbar.java
@@ -0,0 +1,34 @@
+package org.sefaria.sefaria;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+import org.sefaria.sefaria.layouts.SefariaTextView;
+
+/**
+ * Created by nss on 4/11/16.
+ */
+public class HomeActionbar extends LinearLayout {
+
+    public HomeActionbar(Context context, Util.Lang lang, OnClickListener searchClick, OnClickListener langClick) {
+        super(context);
+        inflate(context, R.layout.home_actionbar, this);
+
+        SefariaTextView tv = (SefariaTextView) findViewById(R.id.search_tv);
+        tv.setFont(Util.Lang.EN, true);
+        tv.setOnClickListener(searchClick);
+        findViewById(R.id.search_btn).setOnClickListener(searchClick);
+        findViewById(R.id.lang_btn).setOnClickListener(langClick);
+
+
+        setLang(lang);
+
+    }
+
+    public void setLang(Util.Lang lang) {
+        SefariaTextView tv = (SefariaTextView) findViewById(R.id.langTV);
+        tv.setFont(lang,true);
+        if (lang == Util.Lang.HE) tv.setText("א");
+        else /* if (lang == Util.Lang.EN) */ tv.setText("A");
+    }
+}
