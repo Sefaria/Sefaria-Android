da64fa59700474469824a31a98d58aabfde67a7c
Josh Herzberg <herzbe@cooper.edu>
2017-02-05 15:49:02 -0500

    Will d talmud (#1)
    * add willam D Talmud to TOC and Menu
    
    * add will d tamlud to title bar, and version info to toc

--- app/src/main/java/org/sefaria/sefaria/layouts/SefariaTextView.java
+++ app/src/main/java/org/sefaria/sefaria/layouts/SefariaTextView.java
@@ -2,6 +2,7 @@ package org.sefaria.sefaria.layouts;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.TypedValue;
@@ -33,12 +34,12 @@ public class SefariaTextView extends TextView {
 
         TypedArray a = context.obtainStyledAttributes(attributeSet, R.styleable.SefariaTextView);
         final int N = a.getIndexCount();
+        Integer style = null;
         for(int i = 0; i < N; i++){
             int attr = a.getIndex(i);
             switch(attr){
                 case R.styleable.SefariaTextView_isSerif:
                     isSerif = a.getBoolean(attr,false);
-
                     break;
                 case R.styleable.SefariaTextView_lang:
                     int langInt = a.getInt(attr,0); //Default to english
@@ -46,11 +47,21 @@ public class SefariaTextView extends TextView {
                     else if (langInt == 1) lang = Util.Lang.HE;
                     else if(langInt == 2) lang = Settings.getSystemLang();
                     break;
+                case R.styleable.SefariaTextView_isItalic:
+                    if(a.getBoolean(attr,false))
+                        style = Typeface.ITALIC;
+                    break;
+
             }
         }
         a.recycle();
 
-        setFont(lang, isSerif);
+        if(lang == Util.Lang.HE) {
+            //don't do italics when it's Hebrew b/c it's weird.
+            // Honestly this b/c of william D Talmud and if there's something else that needs it we can chaneg this
+            style = null;
+        }
+        setFont(lang, isSerif, -1, TypedValue.COMPLEX_UNIT_SP, style);
     }
 
 
@@ -65,7 +76,13 @@ public class SefariaTextView extends TextView {
      * @param isSerif
      * @param textSize -1 means keep the textSize the same. else, set to that value in SP
      */
-    public void setFont(Util.Lang lang, boolean isSerif, float textSize) { setFont(lang,isSerif,textSize,TypedValue.COMPLEX_UNIT_SP); }
+    public void setFont(Util.Lang lang, boolean isSerif, float textSize) {
+        setFont(lang, isSerif, textSize, TypedValue.COMPLEX_UNIT_SP);
+    }
+
+    public void setFont(Util.Lang lang, boolean isSerif, float textSize, int typedValue) {
+        setFont(lang, isSerif, textSize, typedValue, null);
+    }
 
     /**
      *
@@ -80,7 +97,7 @@ public class SefariaTextView extends TextView {
      * @param textSize -1 means keep the textSize the same
      * @param typedValue can use TypedValue.COMPLEX_UNIT_PX if the number is in pixels. Useful when using getResources().getDimension() which converts to pixels
      */
-    public void setFont(Util.Lang lang, boolean isSerif, float textSize, int typedValue) {
+    public void setFont(Util.Lang lang, boolean isSerif, float textSize, int typedValue, Integer style) {
         MyApp.Font font;
         if (lang == Util.Lang.HE) {
             if (isSerif) font = MyApp.Font.TAAMEY_FRANK;
@@ -101,7 +118,12 @@ public class SefariaTextView extends TextView {
             }
             //setLineSpacing(0, 1.3f);
         }
-        setTypeface(MyApp.getFont(font));
+        if (style != null){
+            setTypeface(MyApp.getFont(font), style);
+        }else{
+            setTypeface(MyApp.getFont(font));
+        }
+
 
     }
 
