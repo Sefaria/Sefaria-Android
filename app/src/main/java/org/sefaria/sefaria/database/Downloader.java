a8a0ea6584e390ac1cfcf6cb4c542ce529458cc2
Josh Herzberg <joshmherzberg@gmail.com>
2017-04-28 18:49:58 -0400

    3.41: db update

--- app/src/main/java/org/sefaria/sefaria/database/Downloader.java
+++ app/src/main/java/org/sefaria/sefaria/database/Downloader.java
@@ -43,7 +43,7 @@ public class Downloader {
     public static final String CSV_FILE_NAME = "sefaria_mobile_updating_csv.csv";
     private static final String CSV_DEBUG_URL = "http://betamidrash.org/other/app/v2/dev/" + CSV_FILE_NAME; //developing version
     private static final String CSV_TESTER_URL = "http://betamidrash.org/other/app/v2/dev/tester_" + CSV_FILE_NAME; //developing version
-    private static final String CSV_REAL_URL  = CSV_TESTER_URL;//"http://betamidrash.org/other/app/v2/" + CSV_FILE_NAME;
+    private static final String CSV_REAL_URL  = "http://betamidrash.org/other/app/v2/" + CSV_FILE_NAME;
 
     public static final String CSV_DOWNLOAD_TITLE = "Sefaria Pre Update";
     public static final String DB_DOWNLOAD_TITLE = "Sefaria Library Update";
